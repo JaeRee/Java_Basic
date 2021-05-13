7cb0577ecdd1368f70ca9411c1b6902a2dbefd75
JaeHyeoung <84053105+JaeRee@users.noreply.github.com>
2021-05-13 10:31:33 +0900

    add test 20

---
+++ src/Test14.java
@@ -0,0 +1,29 @@
+//	  1
+//	 212
+//	32123
+// 4321234
+//543212345
+
+public class Test14 {
+	
+	public static void main(String[] args) {
+			
+		
+		for(int i=1 ; i<=5 ; i++) {			// Çà
+			
+			for(int j=1 ; j<6-i ; j++)		// ºóÄ­ ¸¸µé±â
+			{
+				System.out.print(" ");
+			}
+			for(int j=i ; j>0 ; j--) {		// 1 21 321 431
+				System.out.print(j);
+			}
+			
+			for(int j=2 ; j<i+1 ; j++) {		// 0 2 23 234 2345
+			}
+			
+			
+			System.out.println();
+		}
+	}
+}
