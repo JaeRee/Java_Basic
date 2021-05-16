14c7c1ed9c760780e4fe72b810412427f5c8bf69
unknown <pjh92528@naver.com>
2021-05-16 22:55:18 +0900

    오늘의 자바

---
+++ src/Plane_Reservation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Plane_Reservation {
+	
+	public static void main(String[] args) {
+		
+		
+		/* 오늘의 자바 : 항공 예약 시스템 만들기 */
+		
+		
+		
+		System.out.println("항공 예약 시스템 만들기");
+		
+		java.util.Scanner sc = new Scanner(System.in);
+		
+		sc.next();
+		
+		
+		
+	}
+}
