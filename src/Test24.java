691712ee91ae8654b7d8ec991c820e2a0d637965
PJH <pjh92528@naver.com>
2021-05-22 23:41:26 +0900

    dhsmfdml zjalt!

---
+++ src/Test24.java
@@ -0,0 +1,15 @@
+
+public class Test24 {
+	public static void main (String[] args) {
+		String star="★";
+		for(int i=0; i<8;i++) {
+			
+				if(i%2 == 0) {
+					
+					star += "★";
+					System.out.println(star);
+				}
+			
+		}
+	}
+}
