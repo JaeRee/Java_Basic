26e4caecf78e9640c7b5341043a61dddd7c5f78d
PJH <pjh92528@naver.com>
2021-05-23 14:28:49 +0900

    REPEAT

--- src/Test24.java
+++ src/Test24.java
@@ -2,14 +2,23 @@
 public class Test24 {
 	public static void main (String[] args) {
 		String star="★";
-		for(int i=0; i<8;i++) {
+		
+		for(int i=0 ; i<5 ; i++) {
 			
-				if(i%2 == 0) {
-					
-					star += "★";
-					System.out.println(star);
-				}
+			
+			for(int j=4-i ; j>0 ; j--){		// 5번 공간, 4, 3, 2, 1, 
+				System.out.print(" ");
+			}
+			
+			for(int j=0 ; j<=i; j++) {		// 1번 찍, 2, 3, 4, 5
+				
+				System.out.print(star);
+			}
+			System.out.println(" ");
 			
 		}
+		
+		
+		
 	}
 }
