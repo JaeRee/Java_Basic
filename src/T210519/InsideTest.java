6ee3fbd9c78757271c4edae8ffb43cf2660a257b
unknown <pjh92528@naver.com>
2021-05-25 03:57:18 +0900

    Inner class

---
+++ src/T210519/InsideTest.java
@@ -0,0 +1,25 @@
+package T210519;
+
+import T210519.Inside.Inside2;
+
+public class InsideTest {
+	
+
+	
+	
+	public static void main(String[] args) {
+		
+			Inside in = new Inside();
+			Inside.Inside2 cal = in.new Inside2();
+			
+			
+			cal.View();
+			
+			System.out.println(cal.value);
+			
+}
+	
+
+
+
+}
